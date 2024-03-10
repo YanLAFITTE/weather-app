0cb22b3b731196fd8eaa15844a783cba26609b33
yala0 <welcome.yanlafitte@gmail.com>
2024-03-10 15:58:01 +0100

    refactoring

--- js/fetchData.js
+++ js/fetchData.js
@@ -1,7 +1,6 @@
-import apiKeyValue from "./conf.js";
+import apiKey from "./conf.js";
 
 const fetchData = async (town) => {
-   const apiKey = apiKeyValue;
    const apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=${town}&appid=${apiKey}&units=metric&lang=fr`;
 
    const response = await fetch(apiUrl);
